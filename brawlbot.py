f99c77b8cf23f8b0e3fe841a85f5284f47312e4e
Michael French <michael.x.french@gmail.com>
2022-06-17 06:11:52 -0500

    everything's working now

--- brawlbot.py
+++ brawlbot.py
@@ -1,5 +1,6 @@
 from pprint import pprint
 import datetime
+import json
 
 import requests
 from discord.ext import commands
@@ -8,7 +9,7 @@ from credentials import DISCORD_TOKEN, BRAWL_TOKEN
 from database import read_json
 from staticip import get_proxy
 
-MYCLUB = 'VCVQPP2'
+MYCLUB = '%23VCVQPP2'
 
 headers = {'Authorization': f'Bearer {BRAWL_TOKEN()}'}
 
@@ -22,37 +23,40 @@ async def on_ready():
         f'{date}: {bot.user} is connected!'
     )
 
-# @bot.command(name="trophy_movers", help="Get the trophy movers for the club. Data is reset whenever Michael gets to it")
-# async def trophy_movers(ctx):
-#     members = client.get_club(MYCLUB).members
-#     old_data = read_json()
+@bot.command(name="trophy_movers", help="Get the trophy movers for the club. Data is reset whenever Michael gets to it")
+async def trophy_movers(ctx):
+    url = f'https://api.brawlstars.com/v1/clubs/{MYCLUB}/members'
+    
+    response = json.loads(requests.get(url, proxies=get_proxy(), headers=headers).text)
+    members = response['items']
+    old_data = read_json()
 
-#     message = f"Trophy movement since {old_data['last_updated']}:\n\n"
+    message = f"Trophy movement since {old_data['last_updated']}:\n\n"
 
-#     for i, member in enumerate(members):
-#         print(f"{i}/{len(members)}")
-#         player = client.get_player(member.tag)
+    for i, member in enumerate(members):
+        player_tag = '%23' + member['tag'][1:]
+        url = f'https://api.brawlstars.com/v1/players/{player_tag}'
         
-#         current_trophies = player.trophies
-#         old_trophies = old_data["data"][i]["trophies"]
-#         trophy_diff = current_trophies - old_trophies
+        print(f"{i}/{len(members)}")
+        player = json.loads(requests.get(url, proxies=get_proxy(), headers=headers).text)
         
-#         arrow = ""
-#         if trophy_diff > 0:
-#             arrow = "⬆"
-#             message += f"{member.name}: {current_trophies} {trophy_diff} {arrow}\n"
-#         elif trophy_diff < 0:
-#             arrow = "⬇"
-#             message += f"{member.name}: {current_trophies} {trophy_diff} {arrow}\n"
-#         else:
-#             continue
+        current_trophies = player["trophies"]
+        old_trophies = old_data["data"][i]["trophies"]
+        trophy_diff = current_trophies - old_trophies
+        
+        
+        arrow = ""
+        if trophy_diff > 0:
+            arrow = "⬆"
+            message += f"{member['name']}: {current_trophies} {trophy_diff} {arrow}\n"
+        elif trophy_diff < 0:
+            arrow = "⬇"
+            message += f"{member['name']}: {current_trophies} {trophy_diff} {arrow}\n"
+        else:
+            continue
         
     
-#     await ctx.send(message)
+    await ctx.send(message)
 
 
-# bot.run(DISCORD_TOKEN())
-
-if __name__ == "__main__":
-    r = requests.get("https://api.brawlstars.com/v1/brawlers", proxies=get_proxy(), headers=headers)
-    pprint(r.text)
+bot.run(DISCORD_TOKEN())
