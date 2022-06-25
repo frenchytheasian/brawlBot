7387041988c8e803cf2c23b49a21c441c25ba905
Michael French <michael.x.french@gmail.com>
2022-06-25 15:07:21 -0500

    read and write to firestore

--- database.py
+++ database.py
@@ -1,40 +1,61 @@
+from turtle import update
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import firestore
+from typing import Dict
+from datetime import date
+import requests
 import json
-import datetime
-from pprint import pprint
 
 from credentials import BRAWL_TOKEN
-MYCLUB = "VCVQPP2"
 
-# def _get_data():
-#     players = dict()
-#     players["data"] = []
-    
-#     members = CLIENT.get_club(MYCLUB).members
-#     for member in members:
-#         player = CLIENT.get_player(member.tag)
-#         player_info = dict()
-#         player_info["tag"] = player.tag
-#         player_info["name"] = player.name
-#         player_info["trophies"] = player.trophies
-        
-#         players["data"].append(player_info)
-#     return players
+MYCLUB = '%23VCVQPP2'
 
-def build_json():
-    players = _get_data()
-    with open("data/data.json", "w") as outfile:
-        players["last_updated"] = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        json.dump(players, outfile)
-        
-def read_json():
-    with open("data/data.json", "r") as infile:
-        data = json.load(infile)
-    return data
+headers = {'Authorization': f'Bearer {BRAWL_TOKEN()}'}
+
+# Use the application default credentials
+cred = credentials.ApplicationDefault()
+firebase_admin.initialize_app(cred, {
+    'projectId': 'brawlbot-ae4dd',
+})
+
+db = firestore.client()
+
+
+def get_data():
+    players = dict()
+    players["data"] = []
     
+    url = f'https://bsproxy.royaleapi.dev/v1/clubs/{MYCLUB}/members'
+    response = json.loads(requests.get(url, headers=headers).text)
+    members = response['items']
+    for member in members:
+        player_tag = '%23' + member['tag'][1:]
+        url = f'https://bsproxy.royaleapi.dev/v1/players/{player_tag}'
+        player =  json.loads(requests.get(url, headers=headers).text)
+        
+        player_info = dict()
+        player_info["tag"] = player["tag"]
+        player_info["name"] = player["name"]
+        player_info["trophies"] = player["trophies"]
+        
+        players["data"].append(player_info)
+    return players
+
+
+def update_db(data: Dict):
+    today = date.today().strftime("%m%d%Y")
+    doc_ref = db.collection('daily_stats').document(today)
+    doc_ref.set(data)
+    
+def read_db():
+    today = date.today().strftime("%m%d%Y")
+    stats_ref = db.collection('daily_stats').document(today)
+    doc = stats_ref.get()
+    return doc.to_dict()
+        
 
 
 if __name__ == "__main__":
-    build_json()
-        
-    
-        
+    print(read_db())
+ 
