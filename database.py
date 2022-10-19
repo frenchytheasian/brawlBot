7e483cadf8a5c50fc6cd9bbf26b2871434aab595
Michael French <michael.x.french@gmail.com>
2022-10-19 12:58:27 -0500

    Created trophy_reset command

--- database.py
+++ database.py
@@ -38,7 +38,6 @@ def get_player_data():
 
     return players
 
-
 def update_db(collection: str = 'daily_stats', data: Dict = get_player_data()):
     today = date.today().strftime("%m%d%Y")
     data['last_updated'] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -46,12 +45,22 @@ def update_db(collection: str = 'daily_stats', data: Dict = get_player_data()):
     response = doc_ref.set(data)
     print(response)
 
-def read_db():
+def read_daily_stats_db():
     today = date.today().strftime("%m%d%Y")
     stats_ref = db.collection('daily_stats').document(today)
     doc = stats_ref.get()
     return doc.to_dict()
 
+def create_tag(discord_id: str, player_tag: str):
+    doc_ref = db.collection('tags').document(discord_id)
+    response = doc_ref.set({'tag': player_tag})
+    print(response)
+    
+def read_tag(discord_id: str):
+    doc_ref = db.collection('tags').document(discord_id)
+    doc = doc_ref.get()
+    return doc.to_dict()['tag']
+    
 
 if __name__ == "__main__":
-    print(read_db())
+    print(read_daily_stats_db())
